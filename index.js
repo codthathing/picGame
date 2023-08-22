0e71066e0569acd1d00248f203ff38cc20399c2a
Uniquecon <akinwunmiolusegun277@gmail.com>
2023-08-21 17:55:30 -0700

    completed functionalities

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ let pictures =["../IMAGE/tunnel.jpg","../IMAGE/unique.jpg","../IMAGE/board1.jpeg
     // "../IMAGE/calculator.jpg","../IMAGE/birthday.jpg","../IMAGE/services.jpeg"
 ]
 
-let showIntro = true
-window.addEventListener("load", ()=> {
-    if(showIntro) {
+let introFromSession = sessionStorage.getItem("introSeen")
+if(!introFromSession) {
+    window.addEventListener("load", ()=> {
         setTimeout(()=> {
+            document.getElementById("introDiv").classList.add("showIntroDiv")
             document.getElementById("introTextDiv").classList.add("showIntroTextDiv")
             setTimeout(()=> {
                 conText([`You choose 2 boxes that display same or different images, 4 times per set.`],
@@ -42,9 +43,16 @@ window.addEventListener("load", ()=> {
             }, 250)
         }, 250)
         setTimeout(()=> {
-            document.getElementById("introDiv").classList.add("showIntroDiv")
+            document.getElementById("introDiv").classList.remove("showIntroDiv")
         }, 11000)
-    }
+    })
+
+}
+
+document.getElementById("canRes").addEventListener("click", ()=> {
+    document.getElementById("resDiv").classList.remove("showResDiv")
+    sessionStorage.setItem("introSeen", true)
+    window.location.reload();
 })
 
 let loadImg = []
@@ -65,42 +73,42 @@ function addFail() {
     return y
 }
 
-function posBox(id, pos) {
+function posBox(idEnt, pos) {
     let posbox = ``
     posbox = `
         <i class="imgPos">${pos}</i>
     `
-    document.getElementById(id).innerHTML += posbox
+    document.getElementById(idEnt).innerHTML += posbox
 }
-function showImgPos(id, clicked) {
+function showImgPos(id, clicked, idEnt) {
     document.getElementById(id).addEventListener("click", ()=> {
         if(clicked) {
             posArray.push(posArray.length)
             clicked = false
-        }
-        for(let x = 0; x < posArray.length; x++) {
-            if(x <= 1) {
-                posNum = 1
-            } if(x <= 3 && x >= 2) {
-                posNum = 2
-            } if(x <= 5 && x >= 4) {
-                posNum = 3
-            } if(x <= 7 && x >= 6) {
-                posNum = 4
+            for(let x = 0; x < posArray.length; x++) {
+                if(x <= 1) {
+                    posNum = 1
+                } if(x <= 3 && x >= 2) {
+                    posNum = 2
+                } if(x <= 5 && x >= 4) {
+                    posNum = 3
+                } if(x <= 7 && x >= 6) {
+                    posNum = 4
+                }
             }
+            posBox(idEnt, posNum)
         }
-        posBox(id, posNum)
     })
 }
-showImgPos("img-box1", posClicked[0])
-showImgPos("img-box2", posClicked[1])
-showImgPos("img-box3", posClicked[2])
-showImgPos("img-box4", posClicked[3])
-showImgPos("img-box5", posClicked[4])
-showImgPos("img-box6", posClicked[5])
-showImgPos("img-box7", posClicked[6])
-showImgPos("img-box8", posClicked[7])
-showImgPos("img-box9", posClicked[8])
+showImgPos("img-box1", posClicked[0], "entBox1")
+showImgPos("img-box2", posClicked[1], "entBox2")
+showImgPos("img-box3", posClicked[2], "entBox3")
+showImgPos("img-box4", posClicked[3], "entBox4")
+showImgPos("img-box5", posClicked[4], "entBox5")
+showImgPos("img-box6", posClicked[5], "entBox6")
+showImgPos("img-box7", posClicked[6], "entBox7")
+showImgPos("img-box8", posClicked[7], "entBox8")
+showImgPos("img-box9", posClicked[8], "entBox9")
 
 function genImg(id, clicked) {
     document.getElementById(id).addEventListener("click", (e) => {
@@ -108,66 +116,44 @@ function genImg(id, clicked) {
             e.target.src = `${pictures[Math.floor(Math.random() * 6)]}`
             loadImg.push(e.target.src)
             clicked = false
-        }
-        for(let x=0; x<loadImg.length; x++) {
-            if(x === 1) {
-                if(loadImg[0] === loadImg[1]) {
-                    let x = 0
-                    x += 1
-                    totalScore.push(x)
-                    document.getElementById("pass").innerHTML = addPass()
-                } else if (loadImg[0] !== loadImg[1]) {
-                    let x = 0
-                    x += 1
-                    totalScore.push(x)
-                    document.getElementById("fail").innerHTML = addFail()
+            for(let x=0; x<loadImg.length; x++) {
+                if(x === 1) {
+                    if(loadImg[0] === loadImg[1]) {
+                        let x = 0
+                        x += 1
+                        totalScore.push(x)
+                        document.getElementById("pass").innerHTML = addPass()
+                    } else if (loadImg[0] !== loadImg[1]) {
+                        let x = 0
+                        x += 1
+                        totalScore.push(x)
+                        document.getElementById("fail").innerHTML = addFail()
+                    }
+                    loadImg = []
                 }
-                loadImg = []
             }
-        }
-        for(let x=0; x<totalScore.length; x++) {
-            if(x === 3) {
-                if(document.getElementById("pass").textContent > document.getElementById("fail").textContent) {
-                    document.getElementById("resText").innerHTML = "You passed this set."
-                } else if(document.getElementById("fail").textContent > document.getElementById("pass").textContent) {
-                    document.getElementById("resText").innerHTML = "You failed this set."
-                } else {
-                    document.getElementById("resText").innerHTML = "You tied this set."
+            for(let x=0; x<totalScore.length; x++) {
+                if(x === 3) {
+                    setTimeout(() => {
+                        let pass = Number(document.getElementById("pass").textContent)
+                        let fail = Number(document.getElementById("fail").textContent)
+                        if(pass > fail) {
+                            document.getElementById("resText").innerHTML = "You passed this set."
+                        } else if(fail > pass) {
+                            document.getElementById("resText").innerHTML = "You failed this set."
+                        } else {
+                            document.getElementById("resText").innerHTML = "You tied this set."
+                        }
+                        document.getElementById("resDiv").classList.add("showResDiv")
+                        setTimeout(() => {
+                            document.getElementById("resTextDiv").classList.add("showTextDiv")
+                        }, 250)
+                    }, 100)
                 }
-                document.getElementById("resDiv").classList.add("showResDiv")
-                setTimeout(()=> {
-                    document.getElementById("resTextDiv").classList.add("showTextDiv")
-                }, 250)
             }
         }
     })
 }
-
-function clearGame(idImage, picClicked, posClicked, idBox) {
-    document.getElementById(idImage).src = ''
-    document.getElementById(idBox).innerHTML = ''
-    picClicked = true
-    posClicked = true
-}
-
-document.getElementById("canRes").addEventListener("click", ()=> {
-    document.getElementById("resDiv").classList.remove("showResDiv")
-    posArray = []
-    loadImg = []
-    totalScore = []
-    document.getElementById("pass").innerHTML = ""
-    document.getElementById("fail").innerHTML = ""
-    // window.location.reload();
-    clearGame("one", allClicked[0], posClicked[0], "img-box1")
-    clearGame("two", allClicked[1], posClicked[1], "img-box2")
-    clearGame("three", allClicked[2], posClicked[2], "img-box3")
-    clearGame("four", allClicked[3], posClicked[3], "img-box4")
-    clearGame("five", allClicked[4], posClicked[4], "img-box5")
-    clearGame("six", allClicked[5], posClicked[5], "img-box6")
-    clearGame("seven", allClicked[6], posClicked[6], "img-box7")
-    clearGame("eight", allClicked[7], posClicked[7], "img-box8")
-    clearGame("nine", allClicked[8], posClicked[8], "img-box9")
-})
 
 genImg("one", allClicked[0])
 genImg("two", allClicked[1])
